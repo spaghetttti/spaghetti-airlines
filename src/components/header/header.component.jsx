704bbcc8c2c0984be82a7f3d3aab89ac795df130
AsilM <asil9802mum@gmail.com>
2021-08-05 18:44:59 +0500

    finished header with minor flaws

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,14 +5,34 @@ import './header.styles.scss';
 import { ReactComponent as Logo} from '../../assets/takeoff.svg'
 
 const HeaderComponent = () => (
-    <div className="header-bar">
-        <div className="logo-container">
-            <Logo className="logo-icon"/>
-        </div>
-        <div className="sign-in-log-in-container">
-            Sign in
-        </div>
-  
+    <div className="header">
+        <nav className="flex flex-jc-sb flex-ai-c change-on-mob">
+            <div className="header-logo flex flex-jc-sb">
+                <a href="/">    
+                    <Logo className="header-logo-icon"/>
+                    <span className="">Spaghetti AirLines</span>
+                    {
+                        //Edit Class to place <span> in center either hide in mob view 
+                    }
+                </a>
+            </div>
+
+            <div className="header-links hide-for-mobile">
+                <a href="/">Home</a><a href="/">Flight</a>
+                <a href="/">Car</a>
+                <a href="/">Hotel</a>
+                <a href="/">Contact Us</a>
+            </div>
+
+            <a className="button button-sign-in" href='/'>Sign in</a>
+            
+            <a href='/' className="header-menu hide-for-desktop">
+                <span></span>
+                <span></span>
+                <span></span>
+
+            </a>
+        </nav>
     </div>
     
 )
--- src/components/main-grid/main-grid.component.jsx
+++ src/components/main-grid/main-grid.component.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import './main-grid.styles.scss'
+import "./main-grid.styles.scss";
+
+import { ReactComponent as Logo} from '../../assets/takeoff.svg'
+
 
 const MainGrid = () => (
-    <p>main grid will be here</p> 
-)
+  <div className="main-grid">
+    <div className="grid-container">
+      <div>
+        <ul>
+          <li>COVID-19 travel restrictions, quarantine & testing information</li>
+          <li>Face coverings required</li>
+        </ul> 
+        <div className="learn-more-link">Learn more &#10095;</div>
+      </div>
+    </div>
+    <div className="grid-container">&#9992; BOOK A FLIGHT</div>
+    <div className="grid-container">&#9745; CHECK IN</div>
+    <div className="grid-container">&#128348; FLIGHT STATUS</div>
+    <div className="adds">
+      <div className="banner"></div>
+    </div>
+
+    <div className="minor-grid">
+      <div className="grid-container">&#9992; BOOK A FLIGHT</div>
+      <div className="grid-container">&#9745; CHECK IN</div>
+      <div className="grid-container">&#128348; FLIGHT STATUS</div>
+      <div className="grid-container">&#128348; FLIGHT STATUS</div>
+    </div>
+    <div className="main-grid">
+        <div className="grid-container_">
+            <div className="grid-container-text">
+                Flying Spaghetti Airlines <br/>
+                <span>Learn about our boarding process, what's available inflight & more</span>
+            </div>
+            <Logo className='grid-container-logo'/>
+        </div>
+    </div>
+  </div>
+);
 
 export default MainGrid;
