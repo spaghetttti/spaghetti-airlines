17959ee74f51fd1b0391a12477ea2c94811a6e7b
AsilM <asil9802mum@gmail.com>
2021-08-06 12:56:42 +0500

    finished kinda menu animation(no actual menu items yet)

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,40 +1,71 @@
-import React from 'react';
+import React from "react";
 
-import './header.styles.scss';
+import "./header.styles.scss";
 
-import { ReactComponent as Logo} from '../../assets/takeoff.svg'
+import "./menu.scss";
 
-const HeaderComponent = () => (
-    <div className="header">
-        <nav className="flex flex-jc-sb flex-ai-c change-on-mob">
-            <div className="header-logo flex flex-jc-sb">
-                <a href="/">    
-                    <Logo className="header-logo-icon"/>
+import { ReactComponent as Logo } from "../../assets/takeoff.svg";
+
+class HeaderComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {addClass: false}
+    }
+    
+    toggle() {
+        this.setState({addClass: !this.state.addClass})
+    }
+    
+    render() {
+        let boxClass = ["hamburger-menu"];
+        let overLayBoxClass = [""];
+        if (this.state.addClass) {
+            boxClass.push('animate');
+            overLayBoxClass.push('overlay');
+            overLayBoxClass.push('fade-in');
+        }
+        else {
+          overLayBoxClass.push('fade-out');
+        }
+
+        return (
+            <div className="header">
+              <div className={overLayBoxClass.join(' ')}></div>
+              <nav className="flex flex-jc-sb flex-ai-c change-on-mob">
+                <div className="header-logo flex flex-jc-sb">
+                  <a href="/">
+                    <Logo className="header-logo-icon" />
                     <span className="">Spaghetti AirLines</span>
                     {
-                        //Edit Class to place <span> in center either hide in mob view 
+                      //Edit Class to place <span> in center either hide in mob view
                     }
+                  </a>
+                </div>
+          
+                <div className="header-links hide-for-mobile">
+                  <a href="/">Home</a>
+                  <a href="/">Flight</a>
+                  <a href="/">Car</a>
+                  <a href="/">Hotel</a>
+                  <a href="/">Contact Us</a>
+                </div>
+          
+                <a className="button button-sign-in" href="/">
+                  Sign in
                 </a>
+                
+                <div className="header-menu hide-for-desktop menu-wrapper">
+                    <div
+                      onClick={this.toggle.bind(this)}
+                      className={boxClass.join(' ')} 
+                    ></div>
+                  </div>
+              </nav>
             </div>
+          )
 
-            <div className="header-links hide-for-mobile">
-                <a href="/">Home</a><a href="/">Flight</a>
-                <a href="/">Car</a>
-                <a href="/">Hotel</a>
-                <a href="/">Contact Us</a>
-            </div>
-
-            <a className="button button-sign-in" href='/'>Sign in</a>
-            
-            <a href='/' className="header-menu hide-for-desktop">
-                <span></span>
-                <span></span>
-                <span></span>
-
-            </a>
-        </nav>
-    </div>
-    
-)
+    }
+};
 
 export default HeaderComponent;
